741d3cf87649aa52ed8f8d484839e71106a7e147
etochavez <etochavez@gmail.com>
2018-10-25 21:10:56 -0500

    socket

--- router/router.ts
+++ router/router.ts
@@ -1,11 +1,12 @@
 
 import { Router, Request, Response } from 'express';
 
+
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
     res.json({
-        mensaje: 'Todo está bien!'
+        mensaje: 'Todo está bien!!'
     });
 })
 
--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,20 @@
 
 import express = require('express');
 import path = require('path');
+import http = require('http');
+import socketIO = require('socket.io');
 
 export default class Server
 {
     public app: express.Application;
     public port: number;
+    public server: http.Server;
+    public io: socketIO.Server;
 
-    constructor(puerto: number){
+    constructor(puerto: number) {
         this.app = express();
+        this.server = http.createServer(this.app);
+        this.io = socketIO(this.server);
         this.port = puerto;
     }
 
@@ -17,7 +23,7 @@ export default class Server
     }
 
     start( callback: Function) {
-        this.app.listen( this.port, callback );
+        this.server.listen( this.port, callback );
         this.publicFolder();
     }
 
